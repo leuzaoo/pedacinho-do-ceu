0332a8725d4a3fab6e95dbb3a3e279f8c9a5dd4a
leuzaoo <leuzaoocontato@gmail.com>
2022-12-14 15:47:00 -0300

    Update script

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
